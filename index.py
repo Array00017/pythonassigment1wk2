812999c456900149e85a1c7f3fc326288dd7a371
Arnold <Array0824@gmail.com>
2023-12-05 19:58:24 -0500

    first commit

---
+++ index.py
@@ -0,0 +1,22 @@
+#first problem solution 
+#Cube Number Test... Print out all cubed numbers up to the total value 1000. Meaning that if the cubed number is over 1000 break the loop.
+
+list_num = []
+
+for nums in range >= 1000 
+break 
+
+print(nums)
+        
+        
+#Second problem Solution 
+#Take in a users input for their age, if they are younger than 18 print kids, if they're 18 to 65 print adults, else print seniors
+
+age = input (what is your age)
+
+if age < 18 
+print (kids)
+if age > 18 and != 65 
+print (adult) 
+else 
+print (senior)
